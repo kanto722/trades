289100f46a11bc3d3e21e0d7a4debcea15273315
Kanto <kanto72@yandex.ru>
2024-06-02 12:36:44 +0300

    finish

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const showMoreButton = document.querySelector('.show-more');
     const leaderboardRows = document.querySelectorAll('.leaderboard-row');
-
-    showMoreButton.addEventListener('click', function() {
-      leaderboardRows.forEach(row => {
-        row.classList.remove('hidden');
+  
+    if (showMoreButton) {
+      showMoreButton.addEventListener('click', function() {
+        const leaderboardRows = document.querySelectorAll('.leaderboard-row');
+        leaderboardRows.forEach(row => {
+          row.classList.remove('hidden');
+        });
+        showMoreButton.style.display = 'none';
       });
-      showMoreButton.style.display = 'none';
-    });
-
+    }
+  
     function checkWindowSize() {
       if (window.innerWidth <= 768) {
         leaderboardRows.forEach((row, index) => {
@@ -24,8 +27,21 @@ document.addEventListener('DOMContentLoaded', function() {
         showMoreButton.style.display = 'none';
       }
     }
-
+  
     checkWindowSize();
-
+  
     window.addEventListener('resize', checkWindowSize);
+      
+    const langSwitch = document.querySelector('.lang-switch');
+  
+    langSwitch.addEventListener('click', function() {
+      this.classList.toggle('active');
+    });
+  
+    document.addEventListener('click', function(event) {
+      if (!langSwitch.contains(event.target)) {
+        langSwitch.classList.remove('active');
+      }
+    });
   });
+  
